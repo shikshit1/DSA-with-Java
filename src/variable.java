758571f29db827adba7b430a3f8b06f5c924b096
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-06-20 00:38:04 +0530

    Variables in Java

---
+++ src/variable.java
@@ -0,0 +1,53 @@
+import java.sql.SQLOutput;
+
+public class variable {
+    static void main(){
+
+        //create // declare
+        int age;
+        //System.out.println(age);
+        //assign value
+        age = 10;
+        age = 11;
+        System.out.println(age);
+
+        //define or intialize
+        int totalMarks = 20;
+        System.out.println(totalMarks);
+
+        //Case Sensitive
+        int weight = 80;
+        int WEIGHT = 90;
+        System.out.println("weight:" + weight);
+        System.out.println("Weight:"+ WEIGHT);
+
+        //starting letter
+        int marks = 10;
+        int MARKS = 11;
+        int _marks = 12;
+        int $marks = 13;
+
+        //Subsequent letters
+        int height = 21;
+        int height_love= 33;
+        int height$12Love = 44;
+        int height_ = 22;
+        int height$ = 23;
+
+        //no reserved keywords
+        //int class = 22;
+
+        //length
+        int helloJeeKaiseHoSaareMyNameIsLoveBabbar = 101;
+        int a = 1;
+        int b=2;
+        int c = 3;
+        //Camecare
+        int myNameIsShikshitKhandelwal = 51;
+        //int totalMarks = 100;
+
+        //constasts
+        int DAYS_IN_YEAR = 365;
+
+    }
+}
