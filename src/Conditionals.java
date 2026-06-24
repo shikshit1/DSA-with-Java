d14c328df144bf68d4003626b7f76258d3e5d60f
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-06-25 00:57:24 +0530

    Conditionals in Java

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -88,5 +88,34 @@ public class Conditionals {
 
 
         //switch statement
+
+          int day = 5;
+          switch(day){
+              case 1 :
+                  System.out.println("monday");
+                  break;
+              case 2:
+                  System.out.println("tue");
+                  break;
+                  case 3 :
+              System.out.println("wed");
+                      break;
+              case 4 :
+                  System.out.println("thrusday");
+                  break;
+              case 5:
+                  System.out.println("fri");
+                  break;
+              case 6 :
+                  System.out.println("sat");
+                  break;
+              case 7:
+                      System.out.println("sun");
+                  break;
+              default:
+                  System.out.println("invalid");
+
+          }
+
     }
 }
