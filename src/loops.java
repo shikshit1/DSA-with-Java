fd15d58fba0154d8e56d1b16364ae18df627bcd6
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-06-26 01:16:53 +0530

    Loops in Java

---
+++ src/loops.java
@@ -0,0 +1,31 @@
+public class loops {
+    static void main() {
+
+//        //first loop
+//        for (int i=1;i<=5;i++)
+//            System.out.println("value of i:" + i);
+//
+//        for (int i=1;i<=10;i++) {
+//            System.out.println("aman is chodu");
+//        }
+
+//        for(int i =1;i<=10;i+=2){
+//            System.out.println(i);
+//        }
+
+//            for(int i=1; i<=4;i++){
+//             for (int j=1 ;j<=4; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        //nested loop
+
+        for(int i =1;i<=3;i++){
+            for(int j =1;j<=3;j++){
+                System.out.println("i= " + i  + " j = " + j);
+            }
+        }
+    }
+}
