449488628cd44e2ce1be59e7541ec48a653e045a
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-06-28 01:13:09 +0530

    Pattern in Java

--- src/Pattern.java
+++ src/Pattern.java
@@ -34,12 +34,125 @@ public class Pattern {
 
 
         //pattern 3   solid right angle triangle
+//
+//          int n =5;
+//          for(int row=1; row<=n;row++){
+//              //for each row -> variables columns
+//              // formula -> col-> 1 -> value of row
+//              for(int col=1; col<=row;col++){
+//                  System.out.print("* ");
+//              }
+//              // move to next line
+//              System.out.println();
+//          }
 
 
-        
+        //pattern 4   solid rombus
+
+//
+//        int n = 5;
+//        for (int row= 1; row<=n;row++){
+//            // for each row-> space , stars
+//
+//            //spaces
+//
+//            for(int col=1;col<=n-row; col++) {
+//                System.out.print(" ");
+//            }
+//
+//                //stars
+//                for(int col=1;col<=n;col++){
+//                    System.out.print("* ");
+//                }
+//                //move to next row
+//                System.out.println();
+//            }
+
+
+        //pattern 5 inverted right angle triangle
+
+//         int n =5;
+//         for(int row=1; row<=n;row++){
+//             // for each row -> varibles of colums
+//             for(int col=1; col<=n-row+1;col++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+
+
+        //pattern 6 solid  pyramid pattern
+
+
+//        int n = 5;
+//        for(int row=1; row<=n; row++){
+//            //for each row-> variable columns
+//
+//            //space
+//            for(int col=1;col<=n-row;col++){
+//                System.out.print("  ");
+//            }
+//            //star
+//            for(int col=1;col<=2*row-1;col++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+        //pattern 7
+
+//        int n = 4;
+//
+//        for (int row = 1; row <= n; row++) {
+//            // for each row-> variable of column
+//            //space
+//
+//            for (int col = 1; col <= row - 1; col++) {
+//                System.out.print("  ");
+//              }
+//
+//                //star
+//                for (int col = 1; col <= 2 * n -2* row +1 ; col++) {
+//                    System.out.print("* ");
+//                }
+//                System.out.println();
+//
+//            }
 
 
 
+        // pattern 8  for hollow pattern
 
+
+
+        int n =4;
+
+        for (int row=1; row<=n; row++){
+            // for each row -> 6 column
+            for (int col=1; col<=6;col++){
+                if(row==1||row==n){
+                    System.out.print("* ");
+                }
+                else {
+                    //middle rows
+                    if(col==1||col==6){
+                        System.out.print("* ");
+                    }
+
+                    else {
+                        // middle column
+                        System.out.print("  ");
+                    }
+                }
+            }
+
+            //move to next row
+            System.out.println();
+        }
+
+
+        }
     }
-}
+
+
