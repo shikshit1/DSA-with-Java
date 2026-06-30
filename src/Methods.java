8f117273db07198504f7aec36964329e1620e16d
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-06-30 18:20:51 +0530

    Methods in Java

---
+++ src/Methods.java
@@ -0,0 +1,19 @@
+public class Methods {
+static void print2katable(){
+    for(int i=1; i<=10;i++){
+        int ans=2*i;
+        System.out.println(ans);
+    }
+}
+
+    static void main() {
+        System.out.println("hi");
+        print2katable();
+        System.out.println("bye");
+    }
+
+
+
+
+
+}
