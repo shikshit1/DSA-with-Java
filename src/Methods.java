ad469eb7e5cb9649fcea6794ed3184259d46f739
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-02 01:23:23 +0530

    Methods in Java

--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,6 @@
 public class Methods {
+
+    // for 2 ki table
 static void print2katable(){
     for(int i=1; i<=10;i++){
         int ans=2*i;
@@ -6,10 +8,63 @@ static void print2katable(){
     }
 }
 
+// for sum
+//static void printsum(int x, int y){
+//    System.out.println(x+y);
+//}
+//
+//
+////for multiplication
+//static void printmultiplication(int a , int b){
+//    int ans = a*b;
+//    System.out.println("RESULT:"+ans);
+//}
+
+
+// non void function
+//    static int add(int p,int q){
+//    int sum=p+q;
+//    return sum;
+//    }
+
+//method overloading
+static int add(int a , int b){
+int sum=a+b;
+return sum;
+}
+
+  static int add(int a , int b , int c){
+    int ans=a+b+c;
+    return ans;
+
+  }
+
     static void main() {
-        System.out.println("hi");
-        print2katable();
-        System.out.println("bye");
+
+    int ans1= add(3,5);
+    int ans2= add(3,5,3);
+        System.out.println("ans1:"+ans1);
+        System.out.println("ans2 is " +ans2);
+
+
+
+
+
+    // non void
+
+//        int result = add(23,332);
+//        System.out.println(result);
+//    //multiple
+//    printmultiplication(32,1);
+      // sum
+//    printsum(4,56);
+
+
+
+        // 2 ki table
+//        System.out.println("hi");
+//        print2katable();
+//        System.out.println("bye");
     }
 
 
