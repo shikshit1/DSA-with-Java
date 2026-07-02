023c3bfb44635b342057e898abcf1d52e360e830
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-03 01:35:01 +0530

    Complete Methods in Java

--- src/Methods.java
+++ src/Methods.java
@@ -28,23 +28,65 @@ public class Methods {
 //    }
 
 //method overloading
+//static int add(int a , int b){
+//int sum=a+b;
+//return sum;
+//}
+//
+//  static int add(int a , int b , int c){
+//    int ans=a+b+c;
+//    return ans;
+//
+//  }
+
+
+//  call by value
+
+//      static void solve(int num){
+//          System.out.println( "inside solve: " + num);
+//          num= num*10;
+//          System.out.println("inside solve: " + num);
+//      }
+//
+
+
+//method and variable scoping
+//         static void printmultiples(){
+//             int value=20;
+//             for(int i=1; i<=10;i++){
+//                 System.out.println(20*i);
+//             }
+//             System.out.println(value);
+//         }
+
+
+
+// printwelcome message
+
+//static void printwelcomemessage(){
+//    System.out.println("welcome to java dsa series");
+//}
+
+
+// add int a and b thaat return the sum
 static int add(int a , int b){
-int sum=a+b;
-return sum;
+    return a+b;
 }
 
-  static int add(int a , int b , int c){
-    int ans=a+b+c;
-    return ans;
-
-  }
-
     static void main() {
+    int result =  add(12,23);
+        System.out.println("sum="+result);
+//    printwelcomemessage();
+//        printmultiples();
+//          int num= 5;
+//        System.out.println("inside main:" + num);
+//        solve(num);
+//        System.out.println("inside main:" + num);
 
-    int ans1= add(3,5);
-    int ans2= add(3,5,3);
-        System.out.println("ans1:"+ans1);
-        System.out.println("ans2 is " +ans2);
+//    int ans1= add(3,5);
+//    int ans2= add(3,5,3);
+//        System.out.println("ans1:"+ans1);
+//        System.out.println("ans2 is " +ans2);
 
 
 
