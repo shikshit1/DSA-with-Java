bd22e4f2a04a32b630ca522a249dd8f9a53de897
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-06 23:42:16 +0530

    learning basic in Strings

--- src/StringBasics/keywords.java
+++ src/StringBasics/keywords.java
@@ -3,11 +3,15 @@ package StringBasics;
 
 public class keywords {
     static void main() {
-        String firstname= "yash";
-        String lastname= new String("khandelwal");
-        System.out.println(firstname+" "+ lastname);
+//        String firstname= "yash";
+//        String lastname= new String("khandelwal");
+//        System.out.println(firstname+" "+ lastname);
+//
+//        System.out.println(firstname.length());
 
-        System.out.println(firstname.length());
+        String str= "yash";
+        str = "nash";
+        System.out.println(str);
     }
 
 
