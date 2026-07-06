7868352c6e7fd88f6841ebb203a72aa66237b4f0
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-07 00:40:54 +0530

    learning comparing string

--- src/StringBasics/keywords.java
+++ src/StringBasics/keywords.java
@@ -2,16 +2,55 @@ package StringBasics;
 
 
 public class keywords {
-    static void main() {
+    static void main(String[] args) {
 //        String firstname= "yash";
 //        String lastname= new String("khandelwal");
 //        System.out.println(firstname+" "+ lastname);
 //
 //        System.out.println(firstname.length());
 
-        String str= "yash";
-        str = "nash";
-        System.out.println(str);
+//        String str= "yash";
+//        str = "nash";
+//        System.out.println(str);
+
+
+
+
+           //comparing string
+
+        //==
+//
+//        String name1= "yash";
+//        String name2= "YASH";   // actual content check nhi krtaa , only reference dhekta hai
+//        if(name1==name2){
+//            System.out.println("both are equal");
+//        }
+//        else{
+//            System.out.println("not equal");
+//        }
+
+
+        // .equals()
+
+//        String name1 = "yash";
+//        String name2 = "YASH";     //letters check krta hai dono same capital hai ya  nhi
+//        if(name1.equals(name2)){
+//            System.out.println("both are equal");
+//        }else{
+//            System.out.println("not equal");
+//        }
+
+
+        //.equalsIgnoreCase
+
+        String name1 = "yash";
+        String name2 = "yash";   // yeh actual content check krta hai
+        if(name1.equalsIgnoreCase(name2)){
+            System.out.println("both are equal");
+        }
+        else {
+            System.out.println("not equal");
+        }
     }
 
 
