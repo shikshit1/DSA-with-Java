0e3bba3376e181a942289768478d9591ed9b67cd
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-08 01:45:17 +0530

    practice in string

--- src/StringBasics/vowels.java
+++ src/StringBasics/vowels.java
@@ -1,4 +1,24 @@
 package StringBasics;
 
-public class vowels {
+public class vowels  {
+
+
+
+
+
+    static int getVowelsCount(String str){
+        int count =0;
+        for(int i =0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch =='a'||ch=='e'||ch=='i'|| ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        return count;
+    }
+    static void main() {
+        String str= "yashfgyuewfuwegruehibchbcchbhc b";
+        System.out.println(getVowelsCount(str));
+
+    }
 }
