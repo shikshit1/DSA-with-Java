5946015a94a526eb1add1e249897d5dfd6d214d2
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-10 19:34:40 +0530

    learn basic maths for dsa use

--- src/BasicMaths/PrimeNumber.java
+++ src/BasicMaths/PrimeNumber.java
@@ -3,11 +3,18 @@ package BasicMaths;
 public class PrimeNumber {
 
     static boolean isPrime(int num) {
-        for (int i = 2; i <= num - 1; i++) {
+
+          // dono me se koisa bhi loop chala skte hai
+        for(int i =2;i*i<=num;i++) {
             if (num % i == 0) {
-                System.out.println("not a prime number");
+                System.out.println("it is not  a palindrome");
                 return false;
-            } else {
+//        for (int i = 2; i <= num-1 ; i++) {
+//            if (num % i == 0) {
+//                System.out.println("not a prime number");
+//                return false;
+            }
+        }
                 System.out.println("it is a prime number");
                 return true;
                 //yaha tabhi ayege jb loop se  bahar niklege
@@ -15,13 +22,11 @@ public class PrimeNumber {
                 // uska mtlb it is a prime number
 
             }
-        }
-        return true;
 
-    }
+
 
     static void main() {
-        int num = 11;
+        int num = 17;
         System.out.println(isPrime(num));
 
     }
