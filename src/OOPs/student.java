c44b24bd15c895d2399f1b0c5f5fa85d9115aa64
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-13 17:23:14 +0530

    learn oops concept in java

--- src/OOPs/student.java
+++ src/OOPs/student.java
@@ -2,6 +2,8 @@ package OOPs;
 
 public class student {
     static void main() {
+        //class declaration
+
         class Car {
             String brand;
             int year;
@@ -10,5 +12,47 @@ public class student {
                 System.out.println("Brand: " + brand + ", Year: " + year);
             }
         }
+
+        //object instantiation
+
+                Car car1 = new Car();  // Creating first object
+                car1.brand = "Toyota";
+                car1.year = 2020;
+                car1.display();
+
+                Car car2 = new Car();  // Creating second object
+                car2.brand = "Ford";
+                car2.year = 2018;
+                car2.display();
+            }
+        }
+
+
+
+        //default constructor
+
+ class Car {
+    String brand;
+    int year;
+
+    Car() {
+        System.out.println("Default constructor called");
     }
 }
+
+          //parameterized constructor
+
+class Cars {
+    String brand;
+    int year;
+
+    Cars(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    void display() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+}
+
