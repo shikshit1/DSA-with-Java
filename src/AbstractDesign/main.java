a3a9cfad61e15100257c0ecb81eb6c76e08ab1b9
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-20 00:58:59 +0530

    learn Abstraction in oops

--- src/AbstractDesign/main.java
+++ src/AbstractDesign/main.java
@@ -1,44 +1,94 @@
+//package AbstractDesign;
+//
+//    abstract  class  Bird{
+//
+//      abstract   void fly();
+//
+//       abstract    void eat();
+//
+//    }
+//
+//    class Sparrow extends Bird{
+//
+//        void fly(){
+//            System.out.println("Sparrow flying");
+//        }
+//        void eat(){
+//            System.out.println("Sparrow eating");
+//        }
+//    }
+//
+//    class Crow extends Bird{
+//
+//        @Override
+//        void fly() {
+//            System.out.println("Crow flying");
+//        }
+//
+//        @Override
+//        void eat() {
+//            System.out.println("Crow eating");
+//        }
+//    }
+//public class main {
+//    static void main() {
+//        Bird b=  new Sparrow();
+//        b.eat();
+//        b.fly();
+//
+//       b = new Crow();
+//        b.fly();
+//        b.eat();
+//    }
+//
+//}
+
+
+      //Interface
 package AbstractDesign;
 
-    abstract  class  Bird{
+interface Bird {
+    void fly();
+    void eat();
+}
 
-      abstract   void fly();
+class Sparrow implements Bird {
 
-       abstract    void eat();
-
+    @Override
+    public void fly() {
+        System.out.println("Sparrow flying");
     }
 
-    class Sparrow extends Bird{
+    @Override
+    public void eat() {
+        System.out.println("Sparrow eating");
+    }
+}
 
-        void fly(){
-            System.out.println("Sparrow flying");
-        }
-        void eat(){
-            System.out.println("Sparrow eating");
-        }
+class Crow implements Bird {
+
+    @Override
+    public void fly() {
+        System.out.println("Crow flying");
     }
 
-    class Crow extends Bird{
-
-        @Override
-        void fly() {
-            System.out.println("Crow flying");
-        }
-
-        @Override
-        void eat() {
-            System.out.println("Crow eating");
-        }
+    @Override
+    public void eat() {
+        System.out.println("Crow eating");
     }
+}
+
 public class main {
-    static void main() {
-        Bird b=  new Sparrow();
-        b.eat();
-        b.fly();
 
-       b = new Crow();
-        b.fly();
+    public static void doBirdStuff(Bird b) {
         b.eat();
+        b.fly();
     }
 
+    public static void main(String[] args) {
+
+        doBirdStuff(new Sparrow());
+
+         doBirdStuff(new Crow());
+    }
 }
