6894082d0781bfc8b27d87e5adf3f4af0b04b67a
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-23 00:42:16 +0530

    learn java collections framework

---
+++ src/JavaCollections.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class JavaCollections {
+    static void main() {
+        //list orr collection-> interface
+
+        //Arraylist -> concrete class
+         ArrayList<Integer> list = new ArrayList<>();
+
+
+         //add method
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+
+
+        //list<Integer> list= new ArrayList<>();
+        //collection<Integer> collection = new ArrayList<>();
+    }
+}
