d9f0d093bfe53ff0d3a9f3d7f0709d328f9d412c
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-23 00:59:50 +0530

    learn java collections framework

--- src/JavaCollections.java
+++ src/JavaCollections.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class JavaCollections {
     static void main() {
@@ -13,9 +14,33 @@ public class JavaCollections {
         list.add(20);
         list.add(30);
         System.out.println(list);
+        list.add(40);
+        System.out.println(list);
+
+        //remove method
+        list.remove(0);
+        System.out.println(list);
+
+        //addALL method
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(101);
+        list2.add(102);
+        list2.add(20);
+        list.addAll(list2);
+        System.out.println(list);
+        //remove all method
+        list.removeAll(list2);
+        System.out.println(list);
+
+       //size method
+        System.out.println(list.size());
+
+        // clear method
+        System.out.println("list 2 :"+ list2);
+        list2.clear();
+        System.out.println(list2.size());
 
 
-        //list<Integer> list= new ArrayList<>();
         //collection<Integer> collection = new ArrayList<>();
     }
 }
