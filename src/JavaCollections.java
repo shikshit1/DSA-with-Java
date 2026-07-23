db2a520a24fce3f05d97d1b5743a20d0bfc508d4
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-07-24 00:38:39 +0530

    learn java collections framework

--- src/JavaCollections.java
+++ src/JavaCollections.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class JavaCollections {
     static void main() {
@@ -41,6 +40,49 @@ public class JavaCollections {
         System.out.println(list2.size());
 
 
-        //collection<Integer> collection = new ArrayList<>();
+        //i want to traverse list using iterator
+        Iterator<Integer> iterator= list.iterator();
+        while(iterator.hasNext()){
+            System.out.println("element:"+ iterator.next());
+        }
+
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(12);
+        list3.add(124);
+        list3.add(31);
+        System.out.println(list3.get(0));
+        System.out.println("before list:"+ list3);// means run only element which is in 0 index
+
+
+        list3.set(0,234);   // it means replace the element in 0 index with 234 element
+        System.out.println("after list:"+list3);
+
+
+
+
+        // toArray    // kisi bhi list ko object  array me convert krne ke liye
+        Object[] arr=  list3.toArray();
+        for(Object obj: arr) {
+            System.out.println(obj);
+        }
+            //contain    // check if there is element present is list then run true otherwise false
+            System.out.println(list3.contains(23));
+
+
+            list.add(1);
+            list.add(5);
+        System.out.println("printing entire list"+ list);
+
+        //sort an arrayList
+        Collections.sort(list);
+        System.out.println("printing entire list"+ list);
+
+        //clone   // create another list same  as other lists
+        ArrayList<Integer> newList = (ArrayList<Integer>)list.clone();
+        System.out.println( "printing entire new list" +newList);
+
+        //ensure capacity
+
     }
 }
