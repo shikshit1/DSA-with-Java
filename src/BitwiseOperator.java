d429f608cdfddeb83bef9ba9d3eee55cac293e96
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-08-03 00:08:51 +0530

    Bitwise operator in java

---
+++ src/BitwiseOperator.java
@@ -0,0 +1,34 @@
+public class BitwiseOperator {
+    static void main() {
+//         int a =5;
+//         int b= 6;
+//
+//
+//        System.out.println(a&b);   //AND
+//        System.out.println(a|b);   //OR
+//        System.out.println(a^b);   //XOR
+//        System.out.println(~a);    //NOT
+
+          //Bitwise left shift
+
+//        int n =5;
+//        for(int i=1;i<34;i++){
+//            n= n << 1;
+//            System.out.println(n);
+//            System.out.println();
+//        }
+
+        //Bitwise right Shift
+
+        int n =100;
+        for(int i =1; i<10;i++){
+            n=n>>1;
+            System.out.println(n);
+            System.out.println();
+        }
+
+
+
+
+    }
+}
