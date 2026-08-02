7cdbc1fb6e18ef0a60ec3f87bcc03abe78b420ca
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-08-03 00:55:13 +0530

    Bitwise operator in java

--- src/BitwiseOperator.java
+++ src/BitwiseOperator.java
@@ -20,13 +20,37 @@ public class BitwiseOperator {
 
         //Bitwise right Shift
 
-        int n =100;
-        for(int i =1; i<10;i++){
+//        int n =100;
+//        for(int i =1; i<10;i++){
+//            n=n>>1;
+//            System.out.println(n);
+//            System.out.println();
+//        }
+
+
+          //check a number is even or odd
+
+//         int n = 23;
+//         if((n&1)==0){
+//             System.out.println("even");
+//         }else{
+//             System.out.println("odd");
+//         }
+
+        //Count set bits in a number
+
+        int n=5;
+        int count=0;
+        while(n!=0){
+            if((n&1)!=0){
+                // agr 0 ke equal nhi hai toh ek set bit mill jayegi
+                count++;
+            }
+            //right shift to remove this bit
             n=n>>1;
-            System.out.println(n);
-            System.out.println();
         }
 
+        System.out.println("Set bit Count: "+ count);
 
 
 
