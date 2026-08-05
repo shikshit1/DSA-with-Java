6f035f5e3ff7c6fe6fefd9a6fd92e0ae0668741f
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-08-06 00:56:02 +0530

    Practice questions of  Bitwise operator in java

--- src/ArrayProblems/java/LinearSearch.java
+++ src/ArrayProblems/java/LinearSearch.java
@@ -1,5 +1,23 @@
 package ArrayProblems.java;
 
 public class LinearSearch {
-    
+    static boolean findTarget(int  arr[], int  target){
+        for(int i =0;i<arr.length;i++){
+        if (arr[i]== target){
+            return true;
+        }
+    }
+        // agar poora array travel ho chuka hai
+        //and ek baar bhi target nahi mile
+        //iska mtlb , target is not present in array
+        return false;
+
 }
+
+    static void main() {
+        int arr[] ={1,32,232,8};
+        boolean ans = findTarget( arr, 8);
+        System.out.println(ans);
+    }
+
+    }
