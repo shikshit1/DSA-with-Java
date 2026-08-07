d705fbea69c6c9562fc2b80131ef366909e0d0b7
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-08-08 00:06:03 +0530

    Solve Arrays Problems

---
+++ src/ArrayProblems/java/SwappingAlternative.java
@@ -0,0 +1,21 @@
+package ArrayProblems.java;
+
+public class SwappingAlternative {
+    static void main() {
+        int arr[] = {1, 2, 3, 4, 5};
+        int n = arr.length;
+
+        for (int i = 0; i < n; i += 2) {
+            if (i + 1 < n) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+
+            }
+        }
+        for(int i =0;i<n;i++){
+            System.out.print(arr[i]+" ");
+
+        }
+    }
+}
