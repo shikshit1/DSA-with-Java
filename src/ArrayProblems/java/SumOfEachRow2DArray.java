3a0bfdd27bd0d5053df324bf5b8fc1114adc11e7
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-08-21 23:16:51 +0530

    Solve Arrays Problems

---
+++ src/ArrayProblems/java/SumOfEachRow2DArray.java
@@ -0,0 +1,10 @@
+package ArrayProblems.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumOfEachRow2DArray {
+    public List<Integer> rowsum(int[][] arr){
+        List<Integer> result = new ArrayList<>();
+    }
+}
