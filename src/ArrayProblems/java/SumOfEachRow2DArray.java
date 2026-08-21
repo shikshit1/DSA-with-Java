eafc53f7e60adc697cdb5d04b9dc851de3e7f79b
Shikshit Khandelwal <shikshitkhandelwal456@gmail.com>
2026-08-21 23:31:49 +0530

    Solve Arrays Problems

--- src/ArrayProblems/java/SumOfEachRow2DArray.java
+++ src/ArrayProblems/java/SumOfEachRow2DArray.java
@@ -6,5 +6,30 @@ import java.util.List;
 public class SumOfEachRow2DArray {
     public List<Integer> rowsum(int[][] arr){
         List<Integer> result = new ArrayList<>();
+        int m= arr.length;
+        int n= arr[0].length;
+         for(int row= 0;row<m;row++){
+             int sum=0;
+             for(int col= 0;col<n;col++){
+                 int value= arr[row][col];
+                 sum= sum+value;
+             }
+             result.add(sum);
+         }
+         return result;
+    }
+
+    static void main() {
+        int[][] arr = {
+                {32, 4, 4},
+                {4, 4, 4},
+                {33,3,3},
+        };
+
+        SumOfEachRow2DArray obj = new SumOfEachRow2DArray();
+
+        List<Integer> result = obj.rowsum(arr);
+
+        System.out.println(result);
     }
 }
